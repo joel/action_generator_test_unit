c62aca54a8c7a1354631946eb4fc94f554ea9a9f
Joel AZEMAR <joel.azemar@gmail.com>
2024-05-27 12:12:02 +0200

    Add Main Spec

---
+++ spec/action_generator_test_unit/main_spec.rb
@@ -0,0 +1,14 @@
+module ActionGeneratorTestUnit
+  RSpec.describe Main do
+    subject(:foo) { described_class.new }
+    describe '#action_generator_test_unit' do
+      context 'when all is good' do
+        let(:input) { 'foo' }
+        let(:result) { 'foo' }
+        it do
+          expect(foo.action_generator_test_unit(input)).to eql(result)
+        end
+      end
+    end
+  end
+end
