64625a3732c47d71f13fe864ad1b9dd23e41ea20
Joel AZEMAR <joel.azemar@gmail.com>
2024-05-27 12:12:08 +0200

    Rubocop

--- lib/action_generator_test_unit/main.rb
+++ lib/action_generator_test_unit/main.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module ActionGeneratorTestUnit
   class Main
     def action_generator_test_unit(input)
--- spec/action_generator_test_unit/main_spec.rb
+++ spec/action_generator_test_unit/main_spec.rb
@@ -1,10 +1,13 @@
+# frozen_string_literal: true
+
 module ActionGeneratorTestUnit
   RSpec.describe Main do
     subject(:foo) { described_class.new }
-    describe '#action_generator_test_unit' do
-      context 'when all is good' do
-        let(:input) { 'foo' }
-        let(:result) { 'foo' }
+    describe "#action_generator_test_unit" do
+      context "when all is good" do
+        let(:input) { "foo" }
+        let(:result) { "foo" }
+
         it do
           expect(foo.action_generator_test_unit(input)).to eql(result)
         end
