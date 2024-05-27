566b7ab14fdbd8d9e9749758b4316d7e76f55b73
Joel AZEMAR <joel.azemar@gmail.com>
2024-05-27 12:11:58 +0200

    Initial Commit

---
+++ lib/action_generator_test_unit.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "action_generator_test_unit/version"
+
+module ActionGeneratorTestUnit
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/action_generator_test_unit_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ActionGeneratorTestUnit do
+  it "has a version number" do
+    expect(ActionGeneratorTestUnit::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
